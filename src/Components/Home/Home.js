a831e1881279fd641d47514cd287f7eae5e8bda8
karolmichonsky <karolmichonsky@gmail.com>
2023-12-20 23:32:05 +0100

    feat: Customize default app and add two components

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Dinemy</h1>
+            <h2>Order food the way <u>YOU</u> like.</h2>
+        </div>
+    );
+};
+
+export default Home;
