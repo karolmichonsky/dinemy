de30b34e273a52e123afcce89ae439730f87650e
karolmichonsky <karolmichonsky@gmail.com>
2023-12-21 17:50:29 +0100

    chore: Integrate Tailwind CSS for styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Dinemy</h1>
+            <h1 className='text-3xl'>Dinemy</h1>
             <h2>Order food the way <u>YOU</u> like.</h2>
         </div>
     );
