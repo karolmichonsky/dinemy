cc687d03a379c6aec2ccdaf0cb99863cd8e4d24e
karolmichonsky <karolmichonsky@gmail.com>
2023-12-25 23:47:03 +0100

    feat: Add image to assets, prototype hero section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import pasta from '../Assets/pasta.png';
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-3xl'>Dinemy</h1>
-            <h2>Order food the way <u>YOU</u> like.</h2>
+        <div className='w-screen h-[800px] flex mx-20 py-64 font-sans'>
+            <div className=' w-1/2 text-center text-3xl py-52'>
+                <h1 className=' '>Order your favorite food with Dinemy. </h1>
+                <p>All restaurants in your area in one place</p>
+            </div>
+            <div>
+                <img src={pasta} alt="" />
+            </div>
         </div>
     );
 };
