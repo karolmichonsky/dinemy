1510c8d6d684771a01e1cd1386727bbe6b331177
karolmichonsky <karolmichonsky@gmail.com>
2023-12-26 13:31:30 +0100

    feat: Import and implement React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Home from './Components/Home';
 import Header from './Components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
