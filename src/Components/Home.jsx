dd97f4012817e8712c51e82627108e3eb4236750
karolmichonsky <karolmichonsky@gmail.com>
2023-12-28 23:23:55 +0100

    chore: Make small hero page updates

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import pasta from '../Assets/pasta.png';
-import hero from '../Assets/hero-background.jpg';
+import hero from '../Assets/hero-background.png';
 
 const heroImg = {
     backgroundImage: `url(${hero})`,
@@ -15,7 +15,7 @@ const Home = () => {
         <div style={heroImg} className='w-screen h-[800px] gap-[500px] flex justify-center items-center font-sans'>
             <div className=' text-center max-w-[400px]'>
                 <h1 className='text-5xl font-bold'>Order your favorite food with <span className=' text-amber-800'>Dinemy</span>. </h1>
-                <h3 className='text-4xl'>All restaurants in your area in <span className='font-bold'>one</span> place</h3>
+                <h3 className='text-4xl'>Best dishes in <span className='font-bold'>one</span> place</h3>
             </div>
             <div className='flex justify-center items-center'>
                 <img src={pasta} alt="" className=' w-[400px] mx-auto'/>
