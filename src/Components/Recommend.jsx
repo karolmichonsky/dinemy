09b3f60dcac33f474a22f46feb7c95fda37fcb70
karolmichonsky <karolmichonsky@gmail.com>
2024-01-06 23:41:09 +0100

    style: Update color palette and spacings

--- src/Components/Recommend.jsx
+++ src/Components/Recommend.jsx
@@ -12,23 +12,23 @@ const recommendedBackground = {
 
 const Recommend = () => {
     return (
-        <div className=' px-72 py-16 bg-slate-100'>
+        <div className=' px-72 py-16 bg-white'>
             <h1 style={recommendedBackground} className='text-5xl font-bold text-center py-2'>Recommendations</h1>
-            <h2 className='text-4xl text-center py-4'>Every week we recommend 3 bestsellers dishes</h2>
+            <h2 className='text-4xl text-center py-4 mb-4'>Every week we recommend 3 bestseller dishes</h2>
             <div className='flex justify-center items-center gap-20 flex-wrap'>
-                <div className='py-12 px-8 shadow-lg rounded-lg bg-white max-w-80 flex flex-col items-center'>
+                <div className='py-12 px-8 shadow-lg rounded-lg bg-slate-100 max-w-[22rem] flex flex-col items-center'>
                     <img src={pasta} alt="" className=' w-72 min-w-40 flex mx-auto' />
                     <h1 className='text-4xl text-center py-2'>Lorem ipsum</h1>
                     <p className='text-center text-xl'>Lorem ipsum dolor sit, amet consectetur</p>
                     <button className='bg-orange-500 rounded-lg drop-shadow-lg text-white text-2xl p-2'>ADD TO CART</button>
                 </div>
-                <div className='py-12 px-8 shadow-lg rounded-lg bg-white max-w-80 flex flex-col items-center'>
+                <div className='py-12 px-8 shadow-lg rounded-lg bg-slate-100 max-w-[22rem] flex flex-col items-center'>
                     <img src={pasta} alt="" className=' w-72 min-w-40 flex mx-auto' />
                     <h1 className='text-4xl text-center py-2'>Lorem ipsum</h1>
                     <p className='text-center text-xl'>Lorem ipsum dolor sit, amet consectetur</p>
                     <button className='bg-orange-500 rounded-lg drop-shadow-lg text-white text-2xl p-2'>ADD TO CART</button>
                 </div>
-                <div className='py-12 px-8 shadow-lg rounded-lg bg-white max-w-80 flex flex-col items-center'>
+                <div className='py-12 px-8 shadow-lg rounded-lg bg-slate-100 max-w-[22rem] flex flex-col items-center'>
                     <img src={pasta} alt="" className=' w-72 min-w-40 flex mx-auto' />
                     <h1 className='text-4xl text-center py-2'>Lorem ipsum</h1>
                     <p className='text-center text-xl'>Lorem ipsum dolor sit, amet consectetur</p>
