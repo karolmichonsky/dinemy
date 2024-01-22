b0a951554045065cc4a783a622b1bbbcd9d8156b
karolmichonsky <karolmichonsky@gmail.com>
2024-01-22 23:37:11 +0100

    style: Replace header list buttons with icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import { Bars3Icon, XMarkIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { ShoppingBagIcon, UserIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
     return (
         <div className='w-screen h-[80px] bg-amber-800 fixed drop-shadow-lg'>
-            <div className='px-7 flex justify-between items-center w-full h-full'>
+            <div className='lg:px-7 flex justify-between items-center w-full h-full'>
                 <div>
-                    <a href="/" className='text-3xl bold text-white font-roboto font-bold'>DINEMY</a>
+                    <a href="/" className='text-3xl bold text-white font-roboto font-bold mx-3'>DINEMY</a>
                 </div>
-                <div className='rounded-2xl w-96 h-10 bg-orange-500 drop-shadow-md px-3 hidden lg:flex'>
+                <div className='rounded-2xl w-36 h-10 bg-orange-500 flex drop-shadow-md px-3 sm:w-72 lg:w-96 duration-300'>
                     <MagnifyingGlassIcon className='w-7' />
                     <input type="text" name="" id="" className=' bg-orange-500 w-full px-3 focus:outline-none text-white'/>
                 </div>
                 <div className='flex'>
-                    <ul className='justify-between hidden lg:flex'>
-                        <li className='px-7 py-2 text-2xl text-white mx-1 hover:bg-black hover:bg-opacity-5 rounded-xl duration-150'>HOME</li>
-                        <li className='px-7 py-2 text-2xl text-white mx-1 hover:bg-black hover:bg-opacity-5 rounded-xl duration-150'>MENU</li>
-                        <li className='px-7 py-2 text-2xl text-white bg-orange-500 rounded-xl drop-shadow-md mx-1 hover:bg-orange-600 duration-150'>LOGIN</li>
-                    </ul>
-                    <div>
-                        <Bars3Icon className=' w-9 lg:hidden' />
-                        <XMarkIcon className=' w-9 hidden' />
+                    <div className='flex'>
+                        <ShoppingBagIcon className=' w-9 bg-white rounded-full drop-shadow-2xl text-orange-500 p-1 lg:mx-2 hover:text-amber-800 duration-300 lg:w-11' />
+                        <UserIcon className='w-9 bg-white rounded-full drop-shadow-2xl text-orange-500 p-1 mx-1 lg:mx-2 hover:text-amber-800 duration-300 lg:w-11' />
                     </div>
                     
                 </div>
