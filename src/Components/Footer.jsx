96bca270c542c7f9ebfe2b59833952426ed8c271
karolmichonsky <karolmichonsky@gmail.com>
2024-02-07 23:47:23 +0100

    style: Fix typos, update website text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,28 +7,28 @@ const Footer = () => {
         <div className='flex flex-wrap text-white bg-amber-800 p-8 lg:px-44 lg:py-16'>
             <div className='flex flex-col px-8 w-1/2'>
                 <h1 className=' text-2xl font-bold'>Dinemy</h1>
-                <h3 className=' text-lg hidden lg:block '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus provident itaque est aspernatur alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus provident itaque est aspernatur alias.</h3>
+                <h3 className=' text-lg hidden lg:block '>Thank you for choosing <b>Dinemy</b> for your food ordering needs! We're dedicated to bringing you the tastiest dishes with convenience at your fingertips.</h3>
                 <p className='flex pt-2'><DevicePhoneMobileIcon className=' w-6 mr-2'/>Rzeszow, Grodzisko 324 </p>
                 <p className='flex pt-2'><MapPinIcon className=' w-6 mr-2'/>+47 928 929 929</p>
             </div>
             <div className='w-1/4'>
-                <h1 className=' text-2xl font-bold'>Links</h1>
+                <h1 className=' text-2xl font-bold'>Service</h1>
                 <ul className=' list-none'>
-                    <a href="/"><li>Home</li></a>
-                    <a href="/"><li>Menu</li></a>
-                    <a href="/"><li>Shopping Cart</li></a>
-                    <a href="/"><li>Support</li></a>
+                    <a href="/"><li>Contact</li></a>
                     <a href="/"><li>Login</li></a>
+                    <a href="/"><li>Mobile</li></a>
+                    <a href="/"><li>My account</li></a>
+                    <a href="/"><li>Home</li></a>
                 </ul>
             </div>
             <div className='w-1/4'>
-                <h1 className=' text-2xl font-bold'>Links</h1>
+                <h1 className=' text-2xl font-bold'>Top cuisines</h1>
                 <ul className=' list-none'>
-                    <a href="/"><li>Home</li></a>
-                    <a href="/"><li>Menu</li></a>
-                    <a href="/"><li>Shopping Cart</li></a>
-                    <a href="/"><li>Support</li></a>
-                    <a href="/"><li>Login</li></a>
+                    <a href="/"><li>Chinese</li></a>
+                    <a href="/"><li>Pizza</li></a>
+                    <a href="/"><li>Kebab</li></a>
+                    <a href="/"><li>Sushi</li></a>
+                    <a href="/"><li>Indian</li></a>
                 </ul>
             </div>
         </div>
