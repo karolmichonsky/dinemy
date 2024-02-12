6cbd40cb36f715a226e3a75bc5edddef56d353df
karolmichonsky <karolmichonsky@gmail.com>
2024-02-12 23:40:17 +0100

    feat: Set up small database

---
+++ src/database/data.js
@@ -0,0 +1,30 @@
+import pasta from '../Assets/pasta.png';
+
+const data = [
+    {
+        img: pasta,
+        title: "Pad Thai",
+        description: "A classic Thai stir-fry dish featuring thin rice noodles tossed with tofu, shrimp, or chicken",
+        price: "17.50",
+        choice: "Chicken, Shrimp, Tofu",
+        rating: 4,
+    },
+    {
+        img: pasta,
+        title: "Panang",
+        description: "Thai dish with tender meat or tofu in a coconut milk-based sauce, boasting a perfect balance of sweet, spicy, and savory flavors",
+        price: "12.99",
+        choice: "Chicken, Beef, Shrimp, Tofu",
+        rating: 4,
+    },
+    {
+        img: pasta,
+        title: "Panang",
+        description: "Al dente pasta coated in a robust tomato-based sauce, complemented by savory herbs",
+        price: "13.25",
+        choice: "Chicken, Vege, Whole wheat",
+        rating: 3,
+    },
+];
+
+export default data;
