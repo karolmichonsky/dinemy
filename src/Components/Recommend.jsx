b32aaba5b72b226cc32f254f73234ece8cdb3974
karolmichonsky <karolmichonsky@gmail.com>
2024-02-13 22:53:51 +0100

    feat: Add prices, update recommend data

--- src/Components/Recommend.jsx
+++ src/Components/Recommend.jsx
@@ -5,27 +5,30 @@ import panang from '../Assets/panang.png';
 
 const RecommendData = [
     {
-        id: 1,
-        img: pasta,
-        name: "Pasta",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-        id: 2,
         img: padthai,
         name: "Pad Thai",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        description: "A classic Thai stir-fry dish featuring thin rice noodles tossed with tofu, shrimp, or chicken",
+        price: "17.50",
+        choice: "Chicken, Shrimp, Tofu",
+        rating: 4,
     },
     {
-        id: 3,
         img: panang,
         name: "Panang",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        description: "Thai dish with tender meat or tofu in a coconut milk-based sauce, boasting a perfect balance of sweet, spicy, and savory flavors",
+        price: "12.99",
+        choice: "Chicken, Beef, Shrimp, Tofu",
+        rating: 4,
     },
-]
+    {
+        img: pasta,
+        name: "Red Pasta",
+        description: "Al dente pasta coated in a robust tomato-based sauce, complemented by savory herbs",
+        price: "13.25",
+        choice: "Chicken, Vege, Whole wheat",
+        rating: 3,
+    },
+];
 
 const Recommend = () => {
     return (
@@ -34,10 +37,11 @@ const Recommend = () => {
             <h3 className='text-lg text-center py-4 pt-2 mb-4 text-gray-400'>Every week we recommend 3 bestseller dishes</h3>
             <div className='flex justify-center items-center gap-20 flex-wrap'>
                 {RecommendData.map((recommend) => (
-                    <div className='py-12 px-8 shadow-lg rounded-xl bg-white max-w-[22rem] flex flex-col items-center group hover:bg-orange-500 duration-500'>
+                    <div className='py-12 px-8 shadow-lg rounded-xl bg-white max-w-[22rem] flex flex-col items-center group hover:bg-orange-500 duration-500 basis-full'>
                         <img src={recommend.img} alt="" className=' w-72 min-w-40 flex mx-auto ease-in-out duration-500' />
-                        <h1 className='text-4xl text-center py-2'>{recommend.name}</h1>
-                        <p className='text-center text-xl py-2'>{recommend.description}</p>
+                        <h1 className='text-4xl font-bold text-center py-2'>{recommend.name}</h1>
+                        <p className='text-center text-xl py-2 flex-1 min-h-40  '>{recommend.description}</p>
+                        <h1 className='text-4xl font-bold text-center py-2 pb-4'>{recommend.price}$</h1>
                         <button className='bg-orange-500 rounded-xl shadow-lg text-white text-2xl p-2 group-hover:bg-amber-800 duration-500'>ADD TO CART</button>
                     </div>
                 ))}
