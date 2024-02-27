66fc39fcde52824a9b3203158a3556adb4e9f7ce
karolmichonsky <karolmichonsky@gmail.com>
2024-02-27 23:08:49 +0100

    feat: Add message for incorrect login

--- src/App.js
+++ src/App.js
@@ -22,9 +22,6 @@ function App() {
         <Banner />
         <Store />
         <Footer />
-        
-
-
       </div>
     </Router>
   );
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -16,6 +16,8 @@ const Login = ({ closeLogin }) => {
     const [loginValue, setLoginValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
 
+    const [loginText, setLoginText] = useState('');
+
     const loginHandle = (event) => {
         setLoginValue(event.target.value);
         console.log(loginValue);
@@ -27,17 +29,19 @@ const Login = ({ closeLogin }) => {
     
     const checkAccount = () => {
         for (let i = 0; i < data.length; i++){
-            if(data[i].email == loginValue && data[i].password == passwordValue){
-                console.log("Login");
+            if(data[i].email === loginValue && data[i].password === passwordValue){
+                setLoginText(" ");
+                break;
             }
             else{
-                console.log('Wrong email or password');
+                setLoginText("Wrong email or password. Try Again.");
             }
         }
     }
 
     const toggleRegister = () => {
         setRegister(register => !register);
+        setLoginText(" ");
 
         if (register){
             setConfirmText('Login');
@@ -52,7 +56,7 @@ const Login = ({ closeLogin }) => {
     return (
         <div className={'z-[1] bg-white pt-0 min-w-full min-h-screen lg:bg-opacity-80 lg:bg-black drop-shadow-md flex justify-center items-center absolute lg:top-0 duration-300'}  onClick={closeLogin} >
             <div className='flex flex-col justify-center mx-auto ' onClick={stopPropagation}>
-                <div className='flex flex-col justify-center items-center mx-auto lg:bg-white lg:h-[350px] lg:w-[450px] lg:rounded-lg'>
+                <div className='flex flex-col justify-center items-center mx-auto lg:bg-white lg:h-[400px] lg:w-[450px] lg:rounded-lg'>
                     <h1 className=' text-4xl text-center font-bold lg:pb-4 drop-shadow-lg'>{confirmText}</h1>
                     <div className='bg-orange-500 flex drop-shadow-md px-3 rounded-2xl w-72 h-10 my-2'>
                         <EnvelopeIcon className='w-8' />
@@ -65,10 +69,11 @@ const Login = ({ closeLogin }) => {
                     </div>
                     {register && <div className='bg-orange-500 flex drop-shadow-md px-3 rounded-2xl w-72 h-10 my-2 '>
                         <LockClosedIcon className=' w-8' />
-                        <input type="password" name="" id="" placeholder='Repeat Password' className='bg-orange-500 w-full px-3 focus:outline-none text-white placeholder:text-gray-300 ' />
+                        <input type="password" name="" id="" placeholder='Confirm Password' className='bg-orange-500 w-full px-3 focus:outline-none text-white placeholder:text-gray-300 ' />
                     </div>}
                         <p className='cursor-pointer pt-2 underline'  onClick={toggleRegister}>{registerText}</p>
                         <button onClick={checkAccount} className='bg-orange-500 flex drop-shadow-md px-3 rounded-2xl w-18 h-10 my-2 text-white items-center text-2xl mx-2 w-28 justify-center hover:bg-amber-800 duration-500'>{confirmText}</button>
+                        <p className=' text-red-600'>{loginText}</p>
                 </div>
 
             </div>
