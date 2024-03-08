74ee63e3ec8e6e3a8ab62cf8f0a256c28b323aa3
karolmichonsky <karolmichonsky@gmail.com>
2024-03-08 23:04:31 +0100

    style: Fix tablet UI styling

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -8,10 +8,10 @@ const ShoppingCart = ({ closeCart }) => {
         e.stopPropagation();
     };
     return (
-        <div className={'z-[1] bg-white pt-0 min-w-full min-h-screen md:bg-opacity-80 md:bg-black drop-shadow-md flex justify-center md:justify-end md:items-center absolute md:top-0'} onClick={closeCart} >
-            <div className='flex flex-col p-2 md:bg-white' onClick={stopPropagation} >
+        <div className={'z-[1] bg-white pt-0 min-w-full min-h-screen sm:bg-opacity-80 sm:bg-black drop-shadow-md flex justify-center sm:justify-end sm:items-center absolute sm:top-0'} onClick={closeCart} >
+            <div className='flex flex-col p-2 sm:bg-white' onClick={stopPropagation} >
                 <XMarkIcon className=' w-12 text-orange-500 cursor-pointer' onClick={closeCart}/>
-                <div className='flex flex-col justify-center items-center mx-auto  md:h-screen md:w-[450px] md:rounded-lg p-8' >
+                <div className='flex flex-col justify-center items-center mx-auto  sm:h-screen sm:w-[450px] sm:rounded-lg p-8' >
                     <img src={cart} alt="" />
                     <h1 className='text-2xl font-bold'>Your cart is empty</h1>
                     <p className='text-xl text-center'>Please browse our menu to add items to your cart</p>
