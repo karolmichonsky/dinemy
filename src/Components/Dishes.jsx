9b701d5c388d6f13299929e96a264eb8d50a80f8
karolmichonsky <karolmichonsky@gmail.com>
2024-03-10 23:43:44 +0100

    style: Update order styling

--- src/Components/Dishes.jsx
+++ src/Components/Dishes.jsx
@@ -13,30 +13,32 @@ const Dishes = () => {
     };
 
     return (
-        <div className=' flex flex-col flex-wrap'>
-            <div className=' pt-20 bg-white drop-shadow-md'>
+        <div className=' flex flex-col flex-wrap justify-center items-center'>
+            <div className=' pt-20 bg-white '>
                 <div className='p-4 flex items-center'>
-                    <MagnifyingGlassIcon className=' w-9 rounded-2xl bg-slate-400 p-2 drop-shadow-lg mx-2' />
                     <ul className=' text-xl flex flex-wrap items-center justify-center'>
                         {Cuisines.map(food => (
-                            <li className='mx-2 cursor-pointer' onClick={handleClick}>{food}</li>
+                            <li className='mx-2 px-2.5 p-1.5 cursor-pointer rounded-2xl hover:bg-gray-100' onClick={handleClick}>{food}</li>
                         ))}
                     </ul>
                 </div>
             </div>
-            {data.map((dishes) => (
-                <div className=' bg-white p-8  drop-shadow-lg rounded-xl m-4 flex'>
-                    <div className='pr-4 w-3/4'>
-                        <h1 className=' text-md font-bold py-1'>{dishes.title}</h1>
-                        <p className='py-1'>{dishes.description}</p>
-                        <p className=' py-1'>{dishes.choice}</p>
-                        <h1 className=' font-bold'>{dishes.price}$</h1>
+            <div className=' flex flex-row flex-wrap mx-12 justify-center'>
+                {data.map((dishes) => (
+                    <div className=' bg-white p-8  drop-shadow-lg rounded-xl m-4 flex hover:bg-gray-100 max-w-2xl justify-center flex-wrap flex-col items-center sm:flex-row'>
+                        <div className='pr-4 w-3/4'>
+                            <h1 className=' text-md font-bold py-1'>{dishes.title}</h1>
+                            <p className='py-1'>{dishes.description}</p>
+                            <p className=' py-1'>{dishes.choice}</p>
+                            <h1 className=' font-bold'>{dishes.price}$</h1>
+                        </div>
+                        <div className='  flex '>
+                            <img src={dishes.img} alt="" className=' max-w-32 h-auto' />
+                        </div>
                     </div>
-                    <div className=' w-1/4'>
-                        <img src={dishes.img} alt="" className='  flex items-center justify-center' />
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
+
 
         </div>
     );
---
+++ src/database/account.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        email: "test@mail.com",
+        password: "1234",
+    },
+    {
+        email: "test2@mail.com",
+        password: "4321",
+    },
+];
+
+export default data;
