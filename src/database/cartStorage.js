e2e147068be9cbfad5c87289af0611d8f1bc1365
karolmichonsky <karolmichonsky@gmail.com>
2024-03-20 22:44:51 +0100

    Refactor: Update sessionStorage to fit cart

--- src/database/cartStorage.js
+++ src/database/cartStorage.js
@@ -1,38 +1,34 @@
 export class cartStorage {
     static init() {
         if (!sessionStorage.getItem("cart")) {
-            sessionStorage.setItem("cart", JSON.stringify({
+            sessionStorage.setItem("cart", JSON.stringify([
                 
-            }));
+            ]));
         }
     }
 
-    static setItem(key, value) {
-        let temp = JSON.parse(sessionStorage.getItem("cart") || "{}");
-        if (temp){
-            temp[key] = value;
-            sessionStorage.setItem("cart", JSON.stringify(temp));
-        }
+    static setItem(item) {
+        let cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+        cart.push(item);
+        sessionStorage.setItem("cart", JSON.stringify(cart));
     }
 
-    static getItem(key) {
-        const item = sessionStorage.getItem("cart");
-        return item ? JSON.parse(item)[key] : null;
+    static getItem(index) {
+        const cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+        return cart[index] || null;
     }
 
     static clear() {
         sessionStorage.clear();
     }
 
-    static removeItem(key) {
-        let temp = JSON.parse(sessionStorage.getItem("cart") || "{}");
-        if (temp){
-            delete temp[key];
-            localStorage.setItem("cart", JSON.stringify(temp));
-        }
+    static removeItem(index) {
+        let cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+        cart.splice(index, 1);
+        sessionStorage.setItem("cart", JSON.stringify(cart));
     }
     
     static consoleLog() {
-        console.log(JSON.parse(sessionStorage.getItem("cart") || "{}"));
+        console.log(JSON.parse(sessionStorage.getItem("cart") || "[]"));
     }
 }
