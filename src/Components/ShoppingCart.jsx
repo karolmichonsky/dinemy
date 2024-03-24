24c1cc764c3940c34996ea0fba3200761da31a61
karolmichonsky <karolmichonsky@gmail.com>
2024-03-24 22:29:41 +0100

    Feat: Finalize cart v1
    Todo: fix overflow
    Todo: add cart live update

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { XMarkIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/solid'
 import cart from '../Assets/cart.png';
 import { Link } from 'react-router-dom';
 import { cartStorage } from '../database/cartStorage';
@@ -16,22 +16,35 @@ const ShoppingCart = ({ closeCart }) => {
         e.stopPropagation();
     };
     return (
-        <div className={'z-[1] bg-white pt-0 min-w-full min-h-screen sm:bg-opacity-80 sm:bg-black drop-shadow-md flex justify-center sm:justify-end sm:items-center absolute sm:top-0'} onClick={closeCart} >
+        <div className={'z-[1] bg-white pt-0 min-w-full min-h-screen sm:bg-opacity-80 sm:bg-black drop-shadow-md flex justify-center sm:justify-end sm:items-center absolute overflow-auto sm:top-0'} onClick={closeCart} >
             <div className='flex flex-col p-2 sm:bg-white' onClick={stopPropagation} >
-                <XMarkIcon className=' w-12 text-orange-500 cursor-pointer' onClick={closeCart}/>
+                <XMarkIcon className=' w-12 text-orange-500 cursor-pointer' onClick={closeCart} />
                 {!cartStatus && <div className='flex flex-col justify-center items-center mx-auto  sm:h-screen sm:w-[450px] sm:rounded-lg p-8' >
                     <img src={cart} alt="" />
                     <h1 className='text-2xl font-bold'>Your cart is empty</h1>
                     <p className='text-xl text-center'>Please browse our menu to add items to your cart</p>
                     <Link to={'/Order'}><button className='bg-orange-500 rounded-xl shadow-lg text-white text-2xl p-2 group-hover:bg-amber-800 duration-500 my-2' onClick={closeCart}>ORDER NOW</button></Link>
                 </div>}
-                {cartStatus && <div className='flex flex-col justify-center items-center mx-auto  sm:h-screen sm:w-[450px] sm:rounded-lg p-8' >
-                {cartStorage.getCart().map((dishes, index) => (
+                {cartStatus && <div className='flex flex-col  mx-auto  sm:h-screen sm:w-[450px] sm:rounded-lg p-8' >
+                    {cartStorage.getCart().map((dishes) => (
+                        <div className='flex flex-col justify-start items-start text-md'>
+                            <h1 className='flex font-bold justify-between w-full'>{dishes.title} <span className=' font-normal'>{dishes.price * dishes.count}$</span></h1>
+                            <div className='flex flex-row items-center mx-auto'>
+                                <MinusIcon className='w-8 cursor-pointer rounded-full bg-slate-100 p-1 m-2' onClick={()=>cartStorage.minusDish(dishes)} />
+                                <h1>{dishes.count}</h1>
+                                <PlusIcon className='w-8 cursor-pointer rounded-full bg-slate-100 p-1 m-2' onClick={()=>cartStorage.plusDish(dishes)}/>
+                            </div>
+                            <hr className=" w-full my-4 bg-gray-300 border-1 " />
+                        </div>
+                    ))}
                     <div>
-                            <h1 className=' text-md font-bold py-1'>{dishes.title}</h1>
+                        <h1 className='flex text-md justify-between w-full py-1/2'>Food cost: <span className=' font-bold'>{cartStorage.getPrice()}$</span></h1>
+                        <h1 className='flex text-md justify-between w-full py-1/2'>Delivery: <span className=' font-bold'>0.00$</span></h1>
+                        <h1 className='flex text-md justify-between w-full py-1'>Total cost: <span className=' font-bold'>{cartStorage.getPrice()}$</span></h1>
                     </div>
-                ))}
-                    <Link to={'/Order'}><button className='bg-orange-500 rounded-xl shadow-lg text-white text-2xl p-2 group-hover:bg-amber-800 duration-500 my-2' onClick={closeCart}>PAYMENT</button></Link>
+                    <div className='flex justify-center'>
+                        <Link to={'/Order'}><button className='bg-orange-500 rounded-xl shadow-lg text-white text-2xl p-2 group-hover:bg-amber-800 duration-500 my-2' onClick={closeCart}>PAY {cartStorage.getPrice()}$</button></Link>
+                    </div>
                 </div>}
             </div>
         </div>
--- src/database/cartStorage.js
+++ src/database/cartStorage.js
@@ -2,14 +2,27 @@ export class cartStorage {
     static init() {
         if (!sessionStorage.getItem("cart")) {
             sessionStorage.setItem("cart", JSON.stringify([
-                
+
             ]));
         }
     }
 
     static setItem(item) {
         let cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
-        cart.push(item);
+        let isAdded = false;
+        for (let i = 0; i < cart.length; i++) {
+            if (item.choice === cart[i].choice) {
+                cart[i].count += 1;
+                sessionStorage.setItem("cart", JSON.stringify(cart));
+                return 0;
+            }
+        }
+        if (!isAdded) {
+            item.count = 1;
+            cart.push(item);
+        }
+        else {
+        }
         sessionStorage.setItem("cart", JSON.stringify(cart));
     }
 
@@ -27,17 +40,46 @@ export class cartStorage {
         cart.splice(index, 1);
         sessionStorage.setItem("cart", JSON.stringify(cart));
     }
-    
-    static consoleLog() {
-        console.log(JSON.parse(sessionStorage.getItem("cart") || "[]"));
-    }
 
     static isEmpty() {
         const cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
         return cart.length === 0;
     }
 
-    static getCart(){
+    static getCart() {
         return JSON.parse(sessionStorage.getItem("cart"));
+    }
+
+    static minusDish(item) {
+        const cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+        for (let i = 0; i < cart.length; i++) {
+            if (item.choice === cart[i].choice) {
+                cart[i].count -= 1;
+                sessionStorage.setItem("cart", JSON.stringify(cart));
+                if (cart[i].count === 0) {
+                    this.removeItem(i);
+                }
+                return 0;
+            }
+        }
+    }
+    static plusDish(item){
+        const cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+        for (let i = 0; i < cart.length; i++) {
+            if (item.choice === cart[i].choice) {
+                cart[i].count += 1;
+                sessionStorage.setItem("cart", JSON.stringify(cart));
+                return 0;
+            }
+        }
+    }
+
+    static getPrice(){
+        const cart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+        let price = 0;
+        for (let i = 0; i < cart.length; i++) {
+            price += cart[i].price * cart[i].count;
+        }
+        return Math.floor(price*100)/100;
     }
 }
