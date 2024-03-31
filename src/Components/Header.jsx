87c12733b484ebd52efb36da94e5fe199ece0678
karolmichonsky <karolmichonsky@gmail.com>
2024-03-31 23:49:33 +0200

    Feat: Prepare search method

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,12 +4,27 @@ import { ShoppingBagIcon, UserIcon } from '@heroicons/react/24/solid'
 import Login from './Login';
 import ShoppingCart from './ShoppingCart';
 import { Link } from 'react-router-dom';
+import data from '../database/data.js'
+
 
 const Header = () => {
     const [showLogin, setShowLogin] = useState(false);
     const [showCart, setShowCart] = useState(false);
     const [isLogged, changeIsLogged] = useState(false);
+
+    const [searchValue, setSearchValue] = useState('');
     
+    let searchResult = [];
+
+    const searchHandle = (event) => {
+        setSearchValue(event.target.value);
+        for(let i = 0; i<data.length; i++){
+            if(data[i].title.includes(searchValue)){
+                searchResult.push(data[i]);
+            }
+        }
+        console.log(searchResult);
+    };
 
     const toggleCart = () => {
         setShowLogin(false);
@@ -32,7 +47,7 @@ const Header = () => {
                     <Link to={'/'}><h1 href="/" className='text-3xl bold text-white font-roboto font-bold mx-3'>DINEMY</h1></Link>
                     <div className='rounded-2xl w-36 h-10 bg-white flex drop-shadow-md px-3 sm:w-72 lg:w-96 duration-300 sm:mx-12'>
                         <MagnifyingGlassIcon className='w-7 text-orange-500' />
-                        <input type="text" name="" id="" placeholder='Search for your favorite food' className=' bg-white w-full px-3 focus:outline-none text-black mr-1'/>
+                        <input type="text" name="" id="" placeholder='Search for your favorite food' onChange={searchHandle} className=' bg-white w-full px-3 focus:outline-none text-black mr-1'/>
                     </div>
                 </div>
                 
