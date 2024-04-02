24caee9b03e754b10fefae0f052da3ac5c817730
karolmichonsky <karolmichonsky@gmail.com>
2024-04-02 22:55:52 +0200

    Feat: Implement final search bar functions

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-import { ShoppingBagIcon, UserIcon } from '@heroicons/react/24/solid'
+import { ShoppingBagIcon, UserIcon, PlusIcon } from '@heroicons/react/24/solid'
 import Login from './Login';
 import ShoppingCart from './ShoppingCart';
 import { Link } from 'react-router-dom';
 import data from '../database/data.js'
+import { cartStorage } from '../database/cartStorage.js'
+
 
 
 const Header = () => {
     const [showLogin, setShowLogin] = useState(false);
     const [showCart, setShowCart] = useState(false);
     const [isLogged, changeIsLogged] = useState(false);
+    const [isSearching, toggleIsSearching] = useState(false);
 
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
@@ -25,17 +28,23 @@ const Header = () => {
         return () => clearTimeout(timeoutId);
     }, [searchValue]);
 
+    const searchFocus = () => {
+        toggleIsSearching(!isSearching);
+    }
+
     const searchHandle = (event) => {
         setSearchValue(event.target.value);
     };
 
 
     const toggleCart = () => {
+        setSearchResult([]);
         setShowLogin(false);
         setShowCart(showLogin => !showLogin);
     };
 
     const toggleLogin = () => {
+        setSearchResult([]);
         setShowCart(false);
         setShowLogin(showCart => !showCart);
     };
@@ -45,29 +54,49 @@ const Header = () => {
     };
 
     return (
-        <div className='w-screen h-[80px] bg-amber-800 fixed drop-shadow-lg z-[2]'>
-            <div className=' flex justify-between items-center w-full h-full px-8'>
-                <div className='flex'>
-                    <Link to={'/'}><h1 href="/" className='text-3xl bold text-white font-roboto font-bold mx-3'>DINEMY</h1></Link>
-                    <div className='rounded-2xl w-36 h-10 bg-white flex drop-shadow-md px-3 sm:w-72 lg:w-96 duration-300 sm:mx-12'>
-                        <MagnifyingGlassIcon className='w-7 text-orange-500' />
-                        <input type="text" value={searchValue} name="" id="" placeholder='Search for your favorite food' onChange={searchHandle} className=' bg-white w-full px-3 focus:outline-none text-black mr-1' />
-                    </div>
-                </div>
-
-                <div className='flex'>
+        <div>
+            <div className='w-screen h-[80px] bg-amber-800 fixed drop-shadow-lg z-[2]'>
+                <div className=' flex justify-between items-center w-full h-full px-8'>
                     <div className='flex'>
-                        <ShoppingBagIcon className=' w-9 bg-white rounded-full drop-shadow-2xl text-orange-500 p-1 lg:mx-2 hover:text-amber-800 duration-300 lg:w-11' onClick={toggleCart} />
-                        <UserIcon className='w-9 bg-white rounded-full drop-shadow-2xl text-orange-500 p-1 mx-1 lg:mx-2 hover:text-amber-800 duration-300 lg:w-11' onClick={toggleLogin} />
+                        <Link to={'/'}><h1 href="/" className='text-3xl bold text-white font-roboto font-bold mx-3'>DINEMY</h1></Link>
+                        <div className='rounded-2xl w-36 h-10 bg-white flex drop-shadow-md px-3 sm:w-72 lg:w-96 duration-300 sm:mx-12'>
+                            <MagnifyingGlassIcon className='w-7 text-orange-500' />
+                            <input type="text" value={searchValue} name="" id="" placeholder='Search for your favorite food' onChange={searchHandle} onFocus={searchFocus} onBlur={searchFocus} className=' bg-white w-full px-3 focus:outline-none text-black mr-1' />
+                        </div>
                     </div>
 
-                </div>
-            </div>
-            {showLogin && !isLogged && <Login closeLogin={toggleLogin} userLogged={toggleIsLogged} />}
-            {showCart && <ShoppingCart closeCart={toggleCart} />}
+                    <div className='flex'>
+                        <div className='flex'>
+                            <ShoppingBagIcon className=' w-9 bg-white rounded-full drop-shadow-2xl text-orange-500 p-1 lg:mx-2 hover:text-amber-800 duration-300 lg:w-11' onClick={toggleCart} />
+                            <UserIcon className='w-9 bg-white rounded-full drop-shadow-2xl text-orange-500 p-1 mx-1 lg:mx-2 hover:text-amber-800 duration-300 lg:w-11' onClick={toggleLogin} />
+                        </div>
 
+                    </div>
+                </div>
+                {showLogin && !isLogged && <Login closeLogin={toggleLogin} userLogged={toggleIsLogged} />}
+                {showCart && <ShoppingCart closeCart={toggleCart} />}
+                {isSearching && <div className=' flex flex-col px-12 py-1 bg-white'>
+                    {searchResult.map((search) => (
+                        <div className='  z-[1] sticky bg-white px-8  drop-shadow-lg rounded-xl mx-4 my-1 flex hover:bg-gray-100 max-w-md justify-center flex-wrap flex-row items-center group'>
+                            <div className='pr-4 w-3/4 flex items-center justify-between'>
+                                <div className='flex flex-row items-center  [&>*]:pr-2'>
+                                    <h1 className=' text-md font-bold py-1'>{search.title}</h1>
+                                    <h1 className=' font-bold'>{search.price}$</h1>
+                                </div>
+
+                                < PlusIcon className="w-8 h-8 cursor-pointer rounded-full bg-slate-100 p-1 m-1 group-hover:bg-slate-200" onClick={() => cartStorage.setItem(search)} />
+                            </div>
+
+
+
+                        </div>
+                    ))}
+                </div>}
+            </div>
 
         </div>
+
+
     );
 };
 
