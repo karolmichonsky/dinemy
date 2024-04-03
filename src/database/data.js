df8851c0cee5fb12034a260f630b8ee8c235df08
karolmichonsky <karolmichonsky@gmail.com>
2024-04-03 23:19:51 +0200

    Chore: Add dish photos

--- src/database/data.js
+++ src/database/data.js
@@ -1,6 +1,13 @@
 import pasta from '../Assets/pasta.png';
 import padthai from '../Assets/padthai.png';
 import panang from '../Assets/panang.png';
+import greencurry from '../Assets/greencurry.png';
+import tomyum from '../Assets/tomyum.png';
+import lasagna from '../Assets/lasagna.png';
+import massaman from '../Assets/massaman.png';
+import spaghetti from '../Assets/spaghetti.png';
+import ramen from '../Assets/ramen.png';
+import sushi from '../Assets/sushi.png';
 
 const data = [
     {
@@ -20,15 +27,7 @@ const data = [
         rating: 4,
     },
     {
-        img: pasta,
-        title: "Red Pasta",
-        description: "Traditional Italian pasta with tomato sauce, onions, garlic, ground meat, and herbs like basil and oregano, creating a savory and flavorful dish.",
-        price: "13.25",
-        choice: "Chicken, Vege, Whole wheat",
-        rating: 3,
-    },
-    {
-        img: pasta,
+        img: greencurry,
         title: "Green Curry",
         description: "A popular Thai dish made with fresh green chillies, coconut milk, bamboo shoots, and a choice of meat or tofu. It's aromatic and spicy.",
         price: "14.99",
@@ -36,7 +35,7 @@ const data = [
         rating: 4,
     },
     {
-        img: pasta,
+        img: tomyum,
         title: "Tom Yum Soup",
         description: "A classic Thai soup known for its distinct hot and sour flavors, fragrant spices and herbs generously used in the broth.",
         price: "10.50",
@@ -44,15 +43,7 @@ const data = [
         rating: 4,
     },
     {
-        img: pasta,
-        title: "Lasagna",
-        description: "An Italian dish made of stacked layers of thin pasta with fillings such as ground meats and tomato sauce, cheese, and seasonings and spices such as garlic, oregano and basil.",
-        price: "14.25",
-        choice: "Beef, Vege",
-        rating: 4,
-    },
-    {
-        img: pasta,
+        img: massaman,
         title: "Massaman Curry",
         description: "A Thai curry that is rich and relatively mild. It's made with meat, potatoes, onions, and peanuts in a coconut milk base.",
         price: "13.99",
@@ -60,14 +51,14 @@ const data = [
         rating: 4,
     },
     {
-        img: pasta,
+        img: spaghetti,
         title: "Spaghetti Bolognese",
         description: "A popular Italian pasta dish made from spaghetti, minced beef, garlic, wine and tomatoes, often seasoned with basil and oregano.",
         price: "12.75",
         choice: "Beef, Vege",
         rating: 4,
     },{
-        img: pasta,
+        img: ramen,
         title: "Tonkotsu Ramen",
         description: "A rich Japanese noodle soup with a pork bone broth, served with thin noodles, slices of pork belly, green onions, and a soft-boiled egg.",
         price: "15.99",
@@ -75,15 +66,7 @@ const data = [
         rating: 4,
     },
     {
-        img: pasta,
-        title: "Chicken Burrito",
-        description: "A hearty Mexican wrap filled with seasoned chicken, rice, beans, cheese, and salsa, all wrapped in a soft flour tortilla.",
-        price: "11.50",
-        choice: "Chicken, Beef, Vegetarian",
-        rating: 4,
-    },
-    {
-        img: pasta,
+        img: sushi,
         title: "Sushi Platter",
         description: "An assortment of fresh sushi, including nigiri and rolls with various fillings like salmon, tuna, avocado, and cucumber.",
         price: "22.00",
@@ -92,10 +75,18 @@ const data = [
     },
     {
         img: pasta,
-        title: "Falafel Wrap",
-        description: "Middle Eastern dish of spiced chickpea patties served in a wrap with lettuce, tomato, and tahini sauce.",
-        price: "9.99",
-        choice: "Classic, Spicy, Garlic",
+        title: "Red Pasta",
+        description: "Traditional Italian pasta with tomato sauce, onions, garlic, ground meat, and herbs like basil and oregano, creating a savory and flavorful dish.",
+        price: "13.25",
+        choice: "Chicken, Vege, Whole wheat",
+        rating: 3,
+    },
+    {
+        img: lasagna,
+        title: "Lasagna",
+        description: "An Italian dish made of stacked layers of thin pasta with fillings such as ground meats and tomato sauce, cheese, and seasonings and spices such as garlic, oregano and basil.",
+        price: "14.25",
+        choice: "Beef, Vege",
         rating: 4,
     }
 ];
